1a82ff39fb0b126f45131e0e428d4fdfe2c2ea2a
Gabriel Vasquez <gabevsqz04@gmail.com>
2019-12-06 14:59:32 -0600

    Conectar a Base de Datos (Nuevo y Modificar)

--- src/lab8_gabrielvasquez/Principal.java
+++ src/lab8_gabrielvasquez/Principal.java
@@ -14,34 +14,34 @@ public class Principal extends javax.swing.JFrame {
         cargarTabla();
     }
 
-    public final void cargarTabla(){
+    public final void cargarTabla() {
         log.conectar();
         try {
             log.query.execute("SELECT Nombre,Edad,Teléfono,Correo,Dirección FROM Contactos");
-            
+
             ResultSet rs = log.query.getResultSet();
 
             DefaultTableModel modelo = (DefaultTableModel) contact_table.getModel();
-            
+
             while (rs.next()) {
                 Object[] row = new Object[5];
                 for (int i = 0; i < row.length; i++) {
                     row[i] = rs.getObject(i + 1);
                 }
                 modelo.addRow(row);
-                
-                Contacto c = new Contacto(rs.getString(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getString(5));
+
+                Contacto c = new Contacto(rs.getString(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5));
                 contactos.add(c);
             }
-            
+
             contact_table.setModel(modelo);
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         log.desconectar();
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -62,18 +62,18 @@ public class Principal extends javax.swing.JFrame {
         nc_edad = new javax.swing.JSpinner();
         jd_modContact = new javax.swing.JDialog();
         jScrollPane3 = new javax.swing.JScrollPane();
-        nc_dir1 = new javax.swing.JTextArea();
+        mc_dir = new javax.swing.JTextArea();
         b_modifyC = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
-        nc_phone1 = new javax.swing.JFormattedTextField();
+        mc_phone = new javax.swing.JFormattedTextField();
         jLabel8 = new javax.swing.JLabel();
-        nc_edad1 = new javax.swing.JSpinner();
+        mc_edad = new javax.swing.JSpinner();
         jLabel9 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
-        nc_name1 = new javax.swing.JTextField();
-        nc_mail1 = new javax.swing.JTextField();
+        mc_name = new javax.swing.JTextField();
+        mc_mail = new javax.swing.JTextField();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
@@ -195,10 +195,10 @@ public class Principal extends javax.swing.JFrame {
                 .addGap(24, 24, 24))
         );
 
-        nc_dir1.setColumns(20);
-        nc_dir1.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
-        nc_dir1.setRows(5);
-        jScrollPane3.setViewportView(nc_dir1);
+        mc_dir.setColumns(20);
+        mc_dir.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
+        mc_dir.setRows(5);
+        jScrollPane3.setViewportView(mc_dir);
 
         b_modifyC.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
         b_modifyC.setText("Cambiar");
@@ -213,7 +213,7 @@ public class Principal extends javax.swing.JFrame {
         jLabel7.setText("Modificar Contacto");
 
         try {
-            nc_phone1.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("####-####")));
+            mc_phone.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("####-####")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
@@ -221,8 +221,8 @@ public class Principal extends javax.swing.JFrame {
         jLabel8.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
         jLabel8.setText("Nombre");
 
-        nc_edad1.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
-        nc_edad1.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
+        mc_edad.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
+        mc_edad.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
 
         jLabel9.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
         jLabel9.setText("Edad");
@@ -236,9 +236,9 @@ public class Principal extends javax.swing.JFrame {
         jLabel12.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
         jLabel12.setText("Dirección");
 
-        nc_name1.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
+        mc_name.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
 
-        nc_mail1.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
+        mc_mail.setFont(new java.awt.Font("Dialog", 0, 13)); // NOI18N
 
         javax.swing.GroupLayout jd_modContactLayout = new javax.swing.GroupLayout(jd_modContact.getContentPane());
         jd_modContact.getContentPane().setLayout(jd_modContactLayout);
@@ -263,11 +263,11 @@ public class Principal extends javax.swing.JFrame {
                                     .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(79, 79, 79)
                                 .addGroup(jd_modContactLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(nc_mail1)
-                                    .addComponent(nc_name1)
-                                    .addComponent(nc_phone1)
+                                    .addComponent(mc_mail)
+                                    .addComponent(mc_name)
+                                    .addComponent(mc_phone)
                                     .addGroup(jd_modContactLayout.createSequentialGroup()
-                                        .addComponent(nc_edad1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(mc_edad, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(0, 0, Short.MAX_VALUE)))))
                         .addGap(0, 30, Short.MAX_VALUE)))
                 .addContainerGap())
@@ -284,19 +284,19 @@ public class Principal extends javax.swing.JFrame {
                 .addGap(37, 37, 37)
                 .addGroup(jd_modContactLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(nc_name1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(mc_name, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(47, 47, 47)
                 .addGroup(jd_modContactLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(nc_edad1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(mc_edad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(48, 48, 48)
                 .addGroup(jd_modContactLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(nc_phone1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(mc_phone, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(57, 57, 57)
                 .addGroup(jd_modContactLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(nc_mail1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(mc_mail, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(45, 45, 45)
                 .addGroup(jd_modContactLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -447,17 +447,17 @@ public class Principal extends javax.swing.JFrame {
 
     private void b_saveNewMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_b_saveNewMouseClicked
         String n = nc_name.getText();
-        int e = (int)nc_edad.getValue();
+        int e = (int) nc_edad.getValue();
         String t = nc_phone.getText();
         String c = nc_mail.getText();
         String d = nc_dir.getText();
-        
+
         if (n.equals("") || t.equals("")) {
             JOptionPane.showMessageDialog(this, "Debe ingresar nombre y número de teléfono como mínimo");
         } else {
             DefaultTableModel modelo = (DefaultTableModel) contact_table.getModel();
-            Object [] row = {
-              n,e,t,c,d  
+            Object[] row = {
+                n, e, t, c, d
             };
             modelo.addRow(row);
             contact_table.setModel(modelo);
@@ -465,17 +465,17 @@ public class Principal extends javax.swing.JFrame {
             try {
                 log.conectar();
                 log.query.execute("INSERT INTO Contactos"
-                    + " (Nombre,Edad,Teléfono,Correo,Dirección)"
-                    + " VALUES ('" + n + "', '" + e + "', '" + t + "', '" + c + "', '" + d + "')");
+                        + " (Nombre,Edad,Teléfono,Correo,Dirección)"
+                        + " VALUES ('" + n + "', '" + e + "', '" + t + "', '" + c + "', '" + d + "')");
                 log.commit();
                 JOptionPane.showMessageDialog(this, "Contacto Guardado");
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
-            
-            Contacto nuevo = new Contacto(n,e,t,c,d);
+
+            Contacto nuevo = new Contacto(n, e, t, c, d);
             contactos.add(nuevo);
-            
+
             log.desconectar();
 
             nc_name.setText("");
@@ -483,21 +483,52 @@ public class Principal extends javax.swing.JFrame {
             nc_phone.setText("");
             nc_mail.setText("");
             nc_dir.setText("");
-        }        
+        }
     }//GEN-LAST:event_b_saveNewMouseClicked
 
     private void b_modifyCMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_b_modifyCMouseClicked
+        String n = mc_name.getText();
+        int e = (int) mc_edad.getValue();
+        String t = mc_phone.getText();
+        String c = mc_mail.getText();
+        String d = mc_dir.getText();
+
+        try {
+            log.conectar();
+            log.query.execute("UPDATE Contactos SET Nombre='" + n + "', Edad='" + e + "', Teléfono='" + t + "', Correo='" + c + "', Dirección='" + d + "' WHERE ID='" + pos+1 + "'");
+            log.commit();
         
+            chosen.setNombre(n);
+            chosen.setEdad(e);
+            chosen.setNumero(t);
+            chosen.setCorreo(c);
+            chosen.setDireccion(d);
+            
+        } catch (SQLException ex) {
+
+        }
+        log.desconectar();
+
     }//GEN-LAST:event_b_modifyCMouseClicked
 
     private void b_modContactMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_b_modContactMouseClicked
-        pos = contact_table.getSelectedRow();
-        
-        
-        jd_modContact.setModal(true);
-        jd_modContact.pack();
-        jd_modContact.setLocationRelativeTo(this);
-        jd_modContact.setVisible(true);
+        try {
+            pos = contact_table.getSelectedRow();
+            chosen = contactos.get(pos);
+
+            mc_name.setText(chosen.getNombre());
+            mc_edad.setValue(chosen.getEdad());
+            mc_phone.setText(chosen.getNumero());
+            mc_mail.setText(chosen.getCorreo());
+            mc_dir.setText(chosen.getDireccion());
+
+            jd_modContact.setModal(true);
+            jd_modContact.pack();
+            jd_modContact.setLocationRelativeTo(this);
+            jd_modContact.setVisible(true);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Debe seleccionar uno de sus contactos");
+        }
     }//GEN-LAST:event_b_modContactMouseClicked
 
     public static void main(String args[]) {
@@ -555,19 +586,19 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JDialog jd_modContact;
     private javax.swing.JDialog jd_newContact;
+    private javax.swing.JTextArea mc_dir;
+    private javax.swing.JSpinner mc_edad;
+    private javax.swing.JTextField mc_mail;
+    private javax.swing.JTextField mc_name;
+    private javax.swing.JFormattedTextField mc_phone;
     private javax.swing.JTextArea nc_dir;
-    private javax.swing.JTextArea nc_dir1;
     private javax.swing.JSpinner nc_edad;
-    private javax.swing.JSpinner nc_edad1;
     private javax.swing.JTextField nc_mail;
-    private javax.swing.JTextField nc_mail1;
     private javax.swing.JTextField nc_name;
-    private javax.swing.JTextField nc_name1;
     private javax.swing.JFormattedTextField nc_phone;
-    private javax.swing.JFormattedTextField nc_phone1;
     // End of variables declaration//GEN-END:variables
     DataBase log = new DataBase("./Datos.accdb");
     ArrayList<Contacto> contactos = new ArrayList();
     int pos;
-    String chosen;
+    Contacto chosen;
 }
